ca4a59b50b3aeea1cabaf19ca4ebd184357c06d8
elise.super <elise.super@gmail.com>
2021-05-12 14:11:39 +0200

    made costs optional

--- strategyproof.py
+++ strategyproof.py
@@ -2,9 +2,10 @@ import random
 import argparse
 from approval_mechanisms import greedy
 from utils import k_set
+import numpy as np
 
 
-def check_strategyproofness(profile, prod_costs, possible_ballots, n, budget):
+def check_strategy_proof(profile, prod_costs, possible_ballots, n, budget):
 
     strategy_proof = True
     for manipulator in range(n):
@@ -34,22 +35,29 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
 
     parser.add_argument('--p', type=int, help='number of product', default=3)
-    parser.add_argument('--C', type=list, help='cost per product', default=[1,1,1])
-    parser.add_argument('--b', type=int, help='budget', default=2)
+    parser.add_argument('--C', type=list, help='cost per product', default=None)
+    parser.add_argument('--C_min', type=int, help='min cost of a product', default=1)
+    parser.add_argument('--C_max', type=int, help='max cost of a product', default=2)
+    parser.add_argument('--b', type=int, help='budget', default=3)
     parser.add_argument('--n', type=int, help='number of voters', default=3)
     parser.add_argument('--k', type=int, help='max number of products on a ballot', default=2)
-    parser.add_argument('--sample_size', type=int, help='number of checks on strategyproofness', default=10)
+    parser.add_argument('--sample_size', type=int, help='number of checks on strategyproofness', default=1000)
 
     args = parser.parse_args()
 
     products = list(map(str, list(range(args.p))))
-    costs = list(map(int, args.C))
+
+    if not args.C:
+        costs = np.random.randint(args.C_min, args.C_max, size=args.p)
+    else:
+        costs = list(map(int, args.C))
+
+    print(costs)
 
     cost_dict = dict(zip(products, costs))
     possible_ballots_ = list(k_set(products, args.k))
     profile_ = random.choices(possible_ballots_, k=args.n)
 
-
-    result = [check_strategyproofness(profile_, cost_dict, possible_ballots_, args.n, args.b) for i in range(args.sample_size)]
-    print(f'The provided situation is strategyproof: {sum(result)/len(result) * 100}%')
+    result = [check_strategy_proof(profile_, cost_dict, possible_ballots_, args.n, args.b) for i in range(args.sample_size)]
+    print(f'The provided situation is strategyproof: {sum(result) / len(result) * 100}%')
 
