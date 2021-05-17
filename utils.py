9cb8a33df6311fe9f17da916f995cdd10aaab7a8
elise.super <elise.super@gmail.com>
2021-05-17 20:49:02 +0200

    restricted the manipulation ballots to just the subsets and adding one extra project

--- utils.py
+++ utils.py
@@ -7,33 +7,20 @@ def k_set(prods, k):
     products being k."""
     return chain.from_iterable(combinations(prods, n) for n in range(1, k + 1))
 
+
 def feas_set(projects, budget, cost_dict): 
-    """
-    Returns the set of feasible approval votes of projects on a ballot. The set is bound by the 
+    """Returns the set of feasible approval votes of projects on a ballot. The set is bound by the
     budget, i.e. a voter cannot choose more projects than fits in the available budget. The input dict 
-    proj_cost represent the projects and their costs.
-    """
-    # feas_proj_combs = []
-    #
-    # for proj_comb in chain.from_iterable(combinations(projects, n) for n in range(1, len(projects) + 1)):
-    #
-    #     cost = sum([cost_dict.get(proj) for proj in proj_comb])
-    #
-    #     if cost <= budget:
-    #         feas_proj_combs.append(proj_comb)
-    #
-    # return feas_proj_combs
+    proj_cost represent the projects and their costs."""
     options = list(chain.from_iterable(combinations(projects, n) for n in range(1, len(projects) + 1)))
     ballot_costs = np.fromiter(map(lambda x: sum(list(map(cost_dict.get, x))), options), dtype=np.int)
     return set(compress(options, ballot_costs <= budget))
 
 
 def create_appr_dict(A, projects): 
-    """
-    Creates a dictionary of projects and the voters that approved of them: {project1: [list of voters], ...}
+    """Creates a dictionary of projects and the voters that approved of them: {project1: [list of voters], ...}
     A: list of tuples with input approval ballots, first in list corresponds to first voter
-    projects: list of projects
-    """
+    projects: list of projects"""
     n = len(A)
     appr_dict = {p:[] for p in projects}
     
