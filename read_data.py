29f43e57fb4eefb93ff64931561bc2b1cf03282c
Willemijn de Hoop <willemijn.dehoop@gmail.com>
2021-05-25 18:24:38 +0200

    added functions for distributions

--- read_data.py
+++ read_data.py
@@ -1,48 +1,132 @@
 import numpy as np 
-from utils import *
+import random as rd
 
-def create_cost_dict(dataset): 
+def read_data(path): 
+    file = open(path)
+    all_lines = file.readlines()
+    return all_lines
+
+def get_costs(dataset): 
     """
     Creates cost dictionary from polish data set. Input is name of data set. 
     Output: {project: cost, ...}
-    """
-    file = open(dataset)
-    all_lines = file.readlines()
-    
+    """    
     cost_dict = {}
 
-    start = all_lines.index("PROJECTS\n")+2
-    stop = all_lines.index("VOTES\n")
+    start = dataset.index("PROJECTS\n")+2
+    stop = dataset.index("VOTES\n")
     
-    for line in all_lines[start:stop]: 
-        proj, cost = line.split(';')[0:2]
-        cost_dict[int(proj)] = int(cost)
+    costs = []
+    projects = []
+    
+    for line in dataset[start:stop]: 
+        projects.append(int(line.split(';')[0]))
+        costs.append(int(line.split(';')[1]))
         
-    return cost_dict
+    return costs, projects
 
-def create_ballots(dataset): 
+def get_ballots(dataset): 
     """
     Creates list of ballots that people voted for. 
     """
     ballots = []
     
-    file = open(dataset)
-    all_lines = file.readlines()
+    start = dataset.index("VOTES\n")+2
     
-    start = all_lines.index("VOTES\n")+2
-    
-    for line in all_lines[start:]: 
+    for line in dataset[start:]: 
         ballot = line.split(';')[4].split(',')
         ballot = list(map(int, ballot))
         ballots.append(ballot)
         
     return ballots
 
-# HOW to call these in a function: 
+def get_votes(dataset): 
+    """
+    Read the amount of votes per project. 
+    """
+    
+    projects = []
+    votes = []
 
-# dataset = 'data/poland_warszawa_2019_ursynow.pb'
-# budget = 2000000    
-# cost_dict = create_cost_dict(dataset)
-# projects = list(cost_dict.keys())
-# profiles = create_ballots(dataset) 
-# possible_ballots = feas_set(projects, budget, cost_dict)
+    start = dataset.index("PROJECTS\n")+2
+    stop = dataset.index("VOTES\n")
+    
+    for line in dataset[start:stop]: 
+        projects.append(int(line.split(';')[0]))
+        votes.append(int(line.split(';')[3]))
+        
+    return votes, projects
+
+def get_budget(dataset): 
+    budget = int(dataset[10][7:-1])
+    return budget
+    
+def exhaustiveness(ballots, cost_dict, budget): 
+    
+    exhaust = []
+    
+    for ballot in ballots: 
+        total_costs = 0
+        for project in ballot: 
+            total_costs += cost_dict[project]
+        exhaust.append(total_costs/budget)
+        
+    return exhaust
+
+def get_distributions(path, n_projects): 
+    dataset = read_data(path)
+    ballots = get_ballots(dataset)
+    budget = get_budget(dataset)
+    votes, projects = get_votes(dataset)
+    costs, projects = get_costs(dataset)
+    cost_dict = {proj:cost for proj,cost in zip(projects,costs)}
+    vote_dict = {proj:vote for proj,vote in zip(projects,votes)}
+    budget_percentage = budget/sum(costs)
+    
+    # Exhaustiveness 
+    exhaust = exhaustiveness(ballots, cost_dict, budget)
+    exh_distr, bins, _ = plt.hist(exhaust, bins=10)
+    exh_values = [(bins[i] + bins[i+1])/2 for i in range(len(bins)-1)]
+    
+    # Popularity 
+    projects_sample = rd.choices(projects, k=n_projects)
+    costs_sample = []
+    votes_sample = []
+    for project in projects_sample: 
+        costs_sample.append(cost_dict[project])
+        votes_sample.append(vote_dict[project])
+        
+    sorted_pairs = sorted(zip(votes_sample, costs_sample), reverse=True)
+    pop_distr, costs = [list(tupl) for tupl in zip(*sorted_pairs)]   
+    
+    return exh_distr, exh_values, pop_distr, costs, budget_percentage
+
+
+def create_synth_profile(path, n_voters, n_projects): 
+    """ 
+    This functiion creates profiles from a pabulib file 
+    """
+    exh_distr, exh_values, pop_distr, costs, budget_percentage = get_distributions(path, n_projects) 
+    projects = [i for i in range(n_projects)]
+    
+    budget = sum(costs) * budget_percentage
+    profile = []
+
+    for i in range(n_voters): 
+        exh = rd.choices(exh_values, exh_distr)[0]
+        max_cost = exh * budget
+        total_cost = 0 
+        ballot = []
+        
+        while True: 
+            project = rd.choices(projects, pop_distr, k=1)[0]
+            ballot.append(project)
+            total_cost += costs[project]
+            
+            if total_cost >= max_cost: 
+                break
+            
+        profile.append(ballot)
+    
+    return profile, costs, budget
+
